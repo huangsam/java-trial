3a8a33a3ba08194dcb28f637f069dd80980124e8
Samuel Huang <samhuang91@gmail.com>
2024-10-06 23:24:43 -0700

    Add checkstyle and fix violations

--- src/main/java/org/huangsam/sample/TrialRunner.java
+++ src/main/java/org/huangsam/sample/TrialRunner.java
@@ -1,10 +1,5 @@
 package org.huangsam.sample;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.huangsam.sample.people.Employee;
-
 public class TrialRunner {
     public static void main(String[] args) {
         System.out.println("Hello world");
--- src/main/java/org/huangsam/sample/people/EmployeeRepository.java
+++ src/main/java/org/huangsam/sample/people/EmployeeRepository.java
@@ -1,13 +1,9 @@
 package org.huangsam.sample.people;
 
 import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.huangsam.sample.people.Employee;
 
 public class EmployeeRepository {
-    private HashMap<Integer, Employee> employeeRecords = new HashMap<>();
+    private final HashMap<Integer, Employee> employeeRecords = new HashMap<>();
 
     public void add(Employee employee) {
         employeeRecords.put(employee.getId(), employee);
