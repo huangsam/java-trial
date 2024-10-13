5539ac82a5bd37996b9ad00780e6095d371a797d
Samuel Huang <samhuang91@gmail.com>
2024-10-12 23:31:20 -0700

    Add JavaDoc to math tests

--- src/test/java/org/huangsam/sample/TestMath.java
+++ src/test/java/org/huangsam/sample/TestMath.java
@@ -4,6 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Getting a grasp of the JUnit suite from a Java perspective.
+ * Trying out the {@code @Test} annotation and static assert
+ * methods. The most common one is {@code assertEquals}.
+ */
 public class TestMath {
     @Test
     void testAdd() {
