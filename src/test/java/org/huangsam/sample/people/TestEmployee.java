2793e93a09f2a71d6b127e759fe6e7e0a64280e1
Samuel Huang <samhuang91@gmail.com>
2024-10-12 18:07:39 -0700

    Update EMP_ vars to TEST_

--- src/test/java/org/huangsam/sample/people/TestEmployee.java
+++ src/test/java/org/huangsam/sample/people/TestEmployee.java
@@ -18,20 +18,21 @@ import org.junit.jupiter.api.Test;
  * @see <a href="https://stackify.com/streams-guide-java-8/">Stackify post</a>
  */
 public class TestEmployee {
-    private static final Employee[] EMP_ARRAY = {
+    private static final Employee[] TEST_ARRAY = {
         new Employee(1, "Jeff Bezos", 100000.0),
         new Employee(2, "Bill Gates", 200000.0),
         new Employee(3, "Mark Zuckerberg", 300000.0)
     };
 
     // https://stackoverflow.com/a/16748184
-    private static final List<Employee> EMP_LIST = Arrays.asList(EMP_ARRAY);
+    private static final List<Employee> TEST_LIST = Arrays.asList(TEST_ARRAY);
 
-    private static final EmployeeRepository EMP_REPO = new EmployeeRepository();
+    private static final EmployeeRepository TEST_REPO = new EmployeeRepository();
 
     @BeforeAll
     static void setup() {
-        EMP_LIST.forEach(EMP_REPO::add);
+        // Ensure findByID returns a valid result
+        TEST_LIST.forEach(TEST_REPO::add);
     }
 
     @Test
@@ -44,7 +45,7 @@ public class TestEmployee {
 
     @Test
     void testSalaryIncrement() {
-        EMP_LIST.forEach(e -> {
+        TEST_LIST.forEach(e -> {
             double originalSalary = e.getSalary();
             e.salaryIncrement(10.0);
             assertEquals(e.getSalary(), originalSalary + 10.0);
@@ -54,21 +55,21 @@ public class TestEmployee {
     @Test
     void testEmployeeIdToEmployeeStream() {
         Integer[] empIds = {1, 2, 3};
-        List<Employee> employeesFromRepo = Stream.of(empIds).map(EMP_REPO::findById).toList();
+        List<Employee> employeesFromRepo = Stream.of(empIds).map(TEST_REPO::findById).toList();
         assertEquals(empIds.length, employeesFromRepo.size());
     }
 
     @Test
     void testCollectStreamThenList() {
-        List<Employee> employees = EMP_LIST.stream().toList();
-        assertEquals(EMP_LIST.size(), employees.size());
+        List<Employee> employees = TEST_LIST.stream().toList();
+        assertEquals(TEST_LIST.size(), employees.size());
     }
 
     @Test
     void testFilterStream() {
         Integer[] employeeIds = {1, 2, 3};
         List<Employee> employees = Stream.of(employeeIds)
-                .map(EMP_REPO::findById)
+                .map(TEST_REPO::findById)
                 .filter(Objects::nonNull)
                 .filter(emp -> emp.getSalary() > 100000.0)
                 .toList();
