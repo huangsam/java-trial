87a3a1fc26b52317500af01ea7060c15e1564c6c
Samuel Huang <samhuang91@gmail.com>
2024-10-12 23:27:18 -0700

    Add rigour around compute error

--- src/main/java/org/huangsam/sample/threading/NumberCruncher.java
+++ src/main/java/org/huangsam/sample/threading/NumberCruncher.java
@@ -1,13 +1,14 @@
 package org.huangsam.sample.threading;
 
 public class NumberCruncher {
+    public static final int ERROR_RESULT = -1;
     private static final long BUILTIN_DELAY = 500L;
 
     public int compute(int input) {
         try {
             Thread.sleep(BUILTIN_DELAY * input);
         } catch (InterruptedException e) {
-            return -1;
+            return ERROR_RESULT;
         }
         return input * input;
     }
