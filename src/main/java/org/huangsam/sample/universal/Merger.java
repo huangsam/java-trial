a56bf2aee36e1d70065325ff1daf568f1cce8455
Samuel Huang <samhuang91@gmail.com>
2024-10-13 17:41:37 -0700

    Add mergeSet method to Merger

---
+++ src/main/java/org/huangsam/sample/universal/Merger.java
@@ -0,0 +1,22 @@
+package org.huangsam.sample.universal;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Merger<T extends Comparable<T>> {
+    public List<T> mergeList(List<T> list1, List<T> list2) {
+        List<T> mergedList = new ArrayList<>(list1.size() + list2.size());
+        mergedList.addAll(list1);
+        mergedList.addAll(list2);
+        return mergedList;
+    }
+
+    public Set<T> mergeSet(Set<T> set1, Set<T> set2) {
+        Set<T> mergedSet = new HashSet<>(set1.size() + set2.size());
+        mergedSet.addAll(set1);
+        mergedSet.addAll(set2);
+        return mergedSet;
+    }
+}
---
+++ src/test/java/org/huangsam/sample/universal/TestMerger.java
@@ -0,0 +1,35 @@
+package org.huangsam.sample.universal;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMerger {
+    private static final Merger<Integer> MERGER = new Merger<>();
+
+    private static final List<Integer> L1 =
+            new ArrayList<>(Arrays.asList(3, 7, 2, 9, 8));
+    private static final List<Integer> L2 =
+            new ArrayList<>(Arrays.asList(1, 10, 4));
+
+    private static final Set<Integer> S1 = new HashSet<>(L1);
+    private static final Set<Integer> S2 = new HashSet<>(L2);
+
+    @Test
+    void testMergeList() {
+        List<Integer> merged = MERGER.mergeList(L1, L2);
+        assertEquals(L1.size() + L2.size(), merged.size());
+    }
+
+    @Test
+    void testMergeSet() {
+        Set<Integer> merged = MERGER.mergeSet(S1, S2);
+        assertEquals(S1.size() + S2.size(), merged.size());
+    }
+}
