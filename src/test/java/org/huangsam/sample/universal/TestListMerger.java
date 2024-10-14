9295977ff40134293c9e8a87fcb0dd2d64efee74
Samuel Huang <samhuang91@gmail.com>
2024-10-13 17:13:23 -0700

    Sort imports in merger tests

--- src/test/java/org/huangsam/sample/universal/TestListMerger.java
+++ src/test/java/org/huangsam/sample/universal/TestListMerger.java
@@ -2,9 +2,9 @@ package org.huangsam.sample.universal;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
