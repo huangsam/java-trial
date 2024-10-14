77a362c6d32768e15906e3781cdfff589507ae50
Samuel Huang <samhuang91@gmail.com>
2024-10-14 09:49:57 -0700

    Remove unused import

--- src/test/java/org/huangsam/sample/universal/TestMerger.java
+++ src/test/java/org/huangsam/sample/universal/TestMerger.java
@@ -2,7 +2,6 @@ package org.huangsam.sample.universal;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
