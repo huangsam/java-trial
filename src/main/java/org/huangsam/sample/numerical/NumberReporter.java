f7c6434520f8b1824174f6ebfd1b6c73a05c4d15
Samuel Huang <samhuang91@gmail.com>
2024-10-16 15:54:15 -0700

    Update NumberReporter.java

--- src/main/java/org/huangsam/sample/numerical/NumberReporter.java
+++ src/main/java/org/huangsam/sample/numerical/NumberReporter.java
@@ -1,11 +1,10 @@
 package org.huangsam.sample.numerical;
 
-import org.huangsam.sample.TrialRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NumberReporter {
-    private static final Logger LOG = LoggerFactory.getLogger(TrialRunner.class);
+    private static final Logger LOG = LoggerFactory.getLogger(NumberReporter.class);
 
     public void report(Integer result, Integer id) {
         LOG.info("Crunched {} with thread {}", result, id);
