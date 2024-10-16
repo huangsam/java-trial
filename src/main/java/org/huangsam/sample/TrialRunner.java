de32c5ca64574418d61cce3920c842573c6686c3
Samuel Huang <samhuang91@gmail.com>
2024-10-15 22:04:49 -0700

    Add SL4J and refactor version catalog

--- src/main/java/org/huangsam/sample/TrialRunner.java
+++ src/main/java/org/huangsam/sample/TrialRunner.java
@@ -3,16 +3,19 @@ package org.huangsam.sample;
 import org.huangsam.sample.numerical.NumberCruncher;
 import org.huangsam.sample.numerical.NumberJob;
 import org.huangsam.sample.numerical.NumberReporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TrialRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(TrialRunner.class);
+
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello world");
+        LOG.info("Hello world");
 
         Thread[] threads = {null, null, null, null, null, null};
 
         for (int i = 0; i < threads.length; i++) {
-            threads[i] = new Thread(
-                    new NumberJob(i, new NumberCruncher(), new NumberReporter()));
+            threads[i] = new Thread(new NumberJob(i, new NumberCruncher(), new NumberReporter()));
             threads[i].start();
         }
 
@@ -20,6 +23,6 @@ public class TrialRunner {
             thread.join();
         }
 
-        System.out.println("Bye world");
+        LOG.info("Bye world");
     }
 }
--- src/main/java/org/huangsam/sample/numerical/NumberReporter.java
+++ src/main/java/org/huangsam/sample/numerical/NumberReporter.java
@@ -1,7 +1,13 @@
 package org.huangsam.sample.numerical;
 
+import org.huangsam.sample.TrialRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class NumberReporter {
+    private static final Logger LOG = LoggerFactory.getLogger(TrialRunner.class);
+
     public void report(Integer result, Integer id) {
-        System.out.println("Crunched " + result + " with thread " + id);
+        LOG.info("Crunched {} with thread {}", result, id);
     }
 }
