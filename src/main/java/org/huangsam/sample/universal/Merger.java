9e728da7c78af92f52bd888c1e32dd7e2034779f
Samuel Huang <samhuang91@gmail.com>
2024-10-16 21:37:51 -0700

    Rename argument to into as

--- src/main/java/org/huangsam/sample/universal/Merger.java
+++ src/main/java/org/huangsam/sample/universal/Merger.java
@@ -7,14 +7,14 @@ import java.util.List;
 import java.util.Set;
 
 public class Merger<T> {
-    public Collection<T> mergeAs(Collection<T> c1, Collection<T> c2, Class<?> to) {
+    public Collection<T> mergeAs(Collection<T> c1, Collection<T> c2, Class<?> as) {
         Collection<T> merged;
-        if (List.class.isAssignableFrom(to)) {
+        if (List.class.isAssignableFrom(as)) {
             merged = new ArrayList<>(c1.size() + c2.size());
-        } else if (Set.class.isAssignableFrom(to)) {
+        } else if (Set.class.isAssignableFrom(as)) {
             merged = new HashSet<>(c1.size() + c2.size());
         } else {
-            throw new IllegalArgumentException("Cannot merge collections as " + to.getName());
+            throw new IllegalArgumentException("Cannot merge collections as " + as.getName());
         }
         merged.addAll(c1);
         merged.addAll(c2);
