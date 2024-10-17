f2fa2c2783fd1489ba1719bbe4eb8f34b3414759
Samuel Huang <samhuang91@gmail.com>
2024-10-16 21:28:29 -0700

    Shorten Merger method to be more robust

--- src/main/java/org/huangsam/sample/universal/Merger.java
+++ src/main/java/org/huangsam/sample/universal/Merger.java
@@ -1,22 +1,23 @@
 package org.huangsam.sample.universal;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Merger<T> {
-    public List<T> mergeList(List<T> list1, List<T> list2) {
-        List<T> mergedList = new ArrayList<>(list1.size() + list2.size());
-        mergedList.addAll(list1);
-        mergedList.addAll(list2);
-        return mergedList;
-    }
-
-    public Set<T> mergeSet(Set<T> set1, Set<T> set2) {
-        Set<T> mergedSet = new HashSet<>(set1.size() + set2.size());
-        mergedSet.addAll(set1);
-        mergedSet.addAll(set2);
-        return mergedSet;
+    public Collection<T> mergeAs(Collection<T> c1, Collection<T> c2, Class<?> to) {
+        Collection<T> merged;
+        if (List.class.isAssignableFrom(to)) {
+            merged = new ArrayList<>(c1.size() + c2.size());
+        } else if (Set.class.isAssignableFrom(to)) {
+            merged = new HashSet<>(c1.size() + c2.size());
+        } else {
+            throw new IllegalArgumentException("Cannot merge collections as " + to.getName());
+        }
+        merged.addAll(c1);
+        merged.addAll(c2);
+        return merged;
     }
 }
--- src/test/java/org/huangsam/sample/universal/TestMerger.java
+++ src/test/java/org/huangsam/sample/universal/TestMerger.java
@@ -2,31 +2,32 @@ package org.huangsam.sample.universal;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMerger {
     private static final Merger<Integer> MERGER = new Merger<>();
 
-    private static final List<Integer> L1 = Arrays.asList(3, 7, 2, 9, 8);
-    private static final List<Integer> L2 = Arrays.asList(1, 10, 4);
-
-    private static final Set<Integer> S1 = new HashSet<>(L1);
-    private static final Set<Integer> S2 = new HashSet<>(L2);
+    private static final Collection<Integer> C1 = Arrays.asList(3, 7, 2, 9, 8);
+    private static final Collection<Integer> C2 = Arrays.asList(1, 10, 4);
 
     @Test
     void testMergeList() {
-        List<Integer> merged = MERGER.mergeList(L1, L2);
-        assertEquals(L1.size() + L2.size(), merged.size());
+        Collection<Integer> mergedSame = MERGER.mergeAs(C1, C1, ArrayList.class);
+        Collection<Integer> mergedDifferent = MERGER.mergeAs(C1, C2, ArrayList.class);
+        assertEquals(C1.size() + C1.size(), mergedSame.size());
+        assertEquals(C1.size() + C2.size(), mergedDifferent.size());
     }
 
     @Test
     void testMergeSet() {
-        Set<Integer> merged = MERGER.mergeSet(S1, S2);
-        assertEquals(S1.size() + S2.size(), merged.size());
+        Collection<Integer> mergedSame = MERGER.mergeAs(C1, C1, HashSet.class);
+        Collection<Integer> mergedDifferent = MERGER.mergeAs(C1, C2, HashSet.class);
+        assertEquals(C1.size(), mergedSame.size());
+        assertEquals(C1.size() + C2.size(), mergedDifferent.size());
     }
 }
