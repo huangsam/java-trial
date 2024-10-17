d9222b27a1c47c25c8e01b614965824728ec9223
Samuel Huang <samhuang91@gmail.com>
2024-10-17 14:25:08 -0700

    Create TestConcurrency.java

---
+++ src/test/java/org/huangsam/sample/TestConcurrency.java
@@ -0,0 +1,123 @@
+package org.huangsam.sample;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Testing Java concurrency primitives.
+ *
+ * @see <a href="https://www.baeldung.com/java-util-concurrent">Baeldung on concurrency</a>
+ * @see <a href="https://www.baeldung.com/java-countdown-latch">Baeldung on countdown</a>
+ * @see <a href="https://www.baeldung.com/java-semaphore">Baeldung on semaphore</a>
+ * @see <a href="https://www.baeldung.com/java-blocking-queue">Baeldung on blocking queue</a>
+ * @see <a href="https://www.baeldung.com/java-delay-queue">Baeldung on delay queue</a>
+ * @see <a href="https://www.baeldung.com/java-concurrent-locks">Baeldung on lock</a>
+ * @see <a href="https://www.baeldung.com/java-phaser">Baeldung on phaser</a>
+ */
+public class TestConcurrency {
+    private static final Logger LOG = LoggerFactory.getLogger(TestConcurrency.class);
+
+    private static final long SLEEP_IN_MS = 250L;
+
+    @Test
+    void testExecutorService() throws InterruptedException, ExecutionException {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        Future<Integer> future = service.submit(() -> {
+            Thread.sleep(SLEEP_IN_MS);
+            return 1;
+        });
+
+        assertEquals(1, future.get());
+
+        service.shutdown();
+    }
+
+    @Test
+    void testScheduledExecutorService() throws InterruptedException, ExecutionException {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        Future<Integer> future = service.schedule(() -> 1, SLEEP_IN_MS, TimeUnit.MILLISECONDS);
+
+        assertEquals(1, future.get());
+
+        service.shutdown();
+    }
+
+    @Test
+    void testCountDownLatch() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(5);
+        List<Thread> workers = Stream.generate(() -> new Thread(new CountWorker(latch))).limit(5).toList();
+        workers.forEach(Thread::start);
+        latch.await();
+        assertEquals(0, latch.getCount());
+    }
+
+    @Test
+    void testCyclicBarrier() {
+        Thread[] threads = {null, null, null};
+
+        CyclicBarrier barrier = new CyclicBarrier(threads.length, () -> {
+            LOG.info("All previous tasks are completed");
+        });
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new CyclicWorker(barrier));
+        }
+
+        if (!barrier.isBroken()) {
+            Arrays.stream(threads).forEach(Thread::start);
+        }
+    }
+}
+
+class CountWorker implements Runnable {
+    private static final Logger LOG = LoggerFactory.getLogger(CountWorker.class);
+
+    private final CountDownLatch latch;
+
+    public CountWorker(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        LOG.debug("Run countdown logic");
+        latch.countDown();
+    }
+}
+
+class CyclicWorker implements Runnable {
+    private static final Logger LOG = LoggerFactory.getLogger(CyclicWorker.class);
+
+    private final CyclicBarrier barrier;
+
+    public CyclicWorker(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            LOG.debug("Wait for barrier");
+            barrier.await();
+            LOG.debug("Barrier is released");
+        } catch (InterruptedException | BrokenBarrierException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+}
