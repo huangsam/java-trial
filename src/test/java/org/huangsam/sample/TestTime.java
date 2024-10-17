94bf20a578d675b60d9e5bdc5109589f9bc0a4f4
Samuel Huang <samhuang91@gmail.com>
2024-10-16 22:34:45 -0700

    Add test for LocalDateTime

--- src/test/java/org/huangsam/sample/TestTime.java
+++ src/test/java/org/huangsam/sample/TestTime.java
@@ -2,7 +2,9 @@ package org.huangsam.sample;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,20 +15,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @see <a href="https://www.baeldung.com/java-8-date-time-intro">Bealdung on time</a>
  */
 public class TestTime {
-    private static final LocalTime SIX = LocalTime.of(6, 0);
+    private static final LocalTime SIX_TIME = LocalTime.of(6, 0);
+
+    private static final LocalDateTime START_DATETIME = LocalDateTime.of(2000, Month.JANUARY, 1, 0, 0);
 
     @Test
     void testLocalTimeOffSet() {
-        assertEquals(LocalTime.parse("06:00"), SIX);
-        assertEquals(LocalTime.parse("07:00"), SIX.plusHours(1));
-        assertEquals(LocalTime.parse("05:00"), SIX.minusHours(1));
+        assertEquals(LocalTime.parse("06:00"), SIX_TIME);
+        assertEquals(LocalTime.parse("07:00"), SIX_TIME.plusHours(1));
+        assertEquals(LocalTime.parse("05:00"), SIX_TIME.minusHours(1));
     }
 
     @Test
     void testLocalTimePortions() {
-        assertEquals(6, SIX.getHour());
-        assertEquals(0, SIX.getMinute());
-        assertTrue(SIX.isBefore(LocalTime.parse("06:01")));
+        assertEquals(6, SIX_TIME.getHour());
+        assertEquals(0, SIX_TIME.getMinute());
+        assertTrue(SIX_TIME.isBefore(LocalTime.parse("06:01")));
     }
 
     @Test
@@ -35,5 +39,12 @@ public class TestTime {
         assertEquals(0, LocalTime.MIN.getMinute());
         assertEquals(23, LocalTime.MAX.getHour());
         assertEquals(59, LocalTime.MAX.getMinute());
+    }
+
+    @Test
+    void testLocalDateTimeOffset() {
+        assertEquals(LocalDateTime.parse("2000-01-01T00:00:00"), START_DATETIME);
+        assertEquals(LocalDateTime.parse("2000-01-02T00:00:00"), START_DATETIME.plusDays(1));
+        assertEquals(LocalDateTime.parse("1999-12-31T00:00:00"), START_DATETIME.minusDays(1));
     }
 }
