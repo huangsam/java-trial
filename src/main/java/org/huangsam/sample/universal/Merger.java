7ecf8d9d4da3750fb4461759f65a2585a9558cca
Samuel Huang <samhuang91@gmail.com>
2024-10-16 21:44:43 -0700

    Add SO link on isAssignableFrom

--- src/main/java/org/huangsam/sample/universal/Merger.java
+++ src/main/java/org/huangsam/sample/universal/Merger.java
@@ -6,9 +6,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Merger<T> {
+public class Merger<T extends Comparable<T>> {
     public Collection<T> mergeAs(Collection<T> c1, Collection<T> c2, Class<?> as) {
         Collection<T> merged;
+
+        // https://stackoverflow.com/a/3949379
         if (List.class.isAssignableFrom(as)) {
             merged = new ArrayList<>(c1.size() + c2.size());
         } else if (Set.class.isAssignableFrom(as)) {
@@ -16,6 +18,7 @@ public class Merger<T> {
         } else {
             throw new IllegalArgumentException("Cannot merge collections as " + as.getName());
         }
+
         merged.addAll(c1);
         merged.addAll(c2);
         return merged;
