a566333c5b8f6ee65a0dacbaa04714f4d043d644
Samuel Huang <samhuang91@gmail.com>
2024-10-19 08:52:51 -0700

    Update SemaphoreRunner.java

--- src/main/java/io/huangsam/trial/runners/SemaphoreRunner.java
+++ src/main/java/io/huangsam/trial/runners/SemaphoreRunner.java
@@ -4,6 +4,8 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class SemaphoreRunner extends AbstractRunner {
+    private static int MAX_ATTEMPTS = 10;
+
     private final Semaphore semaphore;
     private int attempts = 0;
 
@@ -16,11 +18,17 @@ public class SemaphoreRunner extends AbstractRunner {
         boolean acquired = false;
         do {
             try {
-                acquired = semaphore.tryAcquire(50L, TimeUnit.MILLISECONDS);
+                acquired = semaphore.tryAcquire(getTimeout(), TimeUnit.MILLISECONDS);
             } catch (InterruptedException e) {
                 log().error(e.getMessage(), e);
             }
+
             attempts++;
+
+            if (attempts == MAX_ATTEMPTS) {
+                log().warn("Failed to acquire semaphore after {} attempts", attempts);
+                return;
+            }
         } while (!acquired);
 
         super.run();
@@ -35,5 +43,9 @@ public class SemaphoreRunner extends AbstractRunner {
     void afterWork() {
         log().debug("Release semaphore");
         semaphore.release();
+    }
+
+    private long getTimeout() {
+        return (long) (50.0 * Math.pow(2, attempts - 1));
     }
 }
