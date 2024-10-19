da49bce473fc1dfd5e2f715fa77753c1abfa9f9f
Samuel Huang <samhuang91@gmail.com>
2024-10-19 11:59:05 -0700

    Fix up executors assertions

--- src/test/java/io/huangsam/trial/TestExecutors.java
+++ src/test/java/io/huangsam/trial/TestExecutors.java
@@ -8,21 +8,23 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestExecutors {
     @Test
-    void testExecutor() {
+    void testExecutor() throws InterruptedException {
         Invoker invoker = new Invoker();
+
         invoker.execute(() -> {
-            try {
-                Thread.sleep(100L);
-            } catch (InterruptedException ignored) {
-            }
         });
+
+        Thread.sleep(100L);
+
         assertTrue(invoker.isInvoked());
     }
 
@@ -30,28 +32,38 @@ public class TestExecutors {
     void testExecutorService() throws InterruptedException, ExecutionException {
         ExecutorService service = Executors.newFixedThreadPool(2);
         Future<Integer> future1 = service.submit(() -> {
-            Thread.sleep(100L);
+            Thread.sleep(50L);
             return 1;
         });
         Future<Integer> future2 = service.submit(() -> {
-            Thread.sleep(125L);
+            Thread.sleep(50L);
             return 2;
         });
 
         assertEquals(1, future1.get());
         assertEquals(2, future2.get());
 
+        assertFalse(service.isTerminated());
+
         service.shutdown();
+
+        assertTrue(service.awaitTermination(100L, TimeUnit.MILLISECONDS));
+        assertTrue(service.isTerminated());
     }
 
     @Test
     void testScheduledExecutorService() throws InterruptedException, ExecutionException {
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-        Future<Integer> future = service.schedule(() -> 1, 500L, TimeUnit.MILLISECONDS);
+        ScheduledFuture<Integer> future = service.schedule(() -> 1, 50L, TimeUnit.MILLISECONDS);
 
         assertEquals(1, future.get());
 
+        assertFalse(service.isTerminated());
+
         service.shutdown();
+
+        assertTrue(service.awaitTermination(100L, TimeUnit.MILLISECONDS));
+        assertTrue(service.isTerminated());
     }
 
     private static class Invoker implements Executor {
