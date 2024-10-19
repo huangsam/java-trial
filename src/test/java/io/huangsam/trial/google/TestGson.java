10e1f49fbeb0a51e978b6aba6b8502dcd411d8b6
Samuel Huang <samhuang91@gmail.com>
2024-10-19 16:11:27 -0700

    Add JavaDoc for Gson tests

--- src/test/java/io/huangsam/trial/google/TestGson.java
+++ src/test/java/io/huangsam/trial/google/TestGson.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Tests basic JSON serialization and deserialization operations from the Google Gson
+ * library. The other popular library is Jackson.
+ *
+ * @see <a href="https://github.com/google/gson/blob/main/UserGuide.md">Gson guide</a>
+ * @see <a href="https://github.com/FasterXML/jackson-docs">Jackson guide</a>
+ */
 public class TestGson {
     private static final Gson GSON = new Gson();
 
