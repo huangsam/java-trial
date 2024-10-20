ad7f8be0e02a93ffa246dd291fe50d892873f324
Samuel Huang <samhuang91@gmail.com>
2024-10-20 14:41:51 -0700

    Add more collection types to Merger

--- src/main/java/io/huangsam/trial/universal/Merger.java
+++ src/main/java/io/huangsam/trial/universal/Merger.java
@@ -3,21 +3,23 @@ package io.huangsam.trial.universal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.TreeSet;
 
 public class Merger<T extends Comparable<T>> {
     public Collection<T> mergeAs(Collection<T> c1, Collection<T> c2, Class<?> as) {
         Collection<T> merged;
 
-        // https://stackoverflow.com/a/3949379
-        if (List.class.isAssignableFrom(as)) {
-            merged = new ArrayList<>(c1.size() + c2.size());
-        } else if (Set.class.isAssignableFrom(as)) {
-            merged = new HashSet<>(c1.size() + c2.size());
-        } else {
-            throw new IllegalArgumentException("Cannot merge collections as " + as.getName());
-        }
+        String className = as.getName();
+        merged = switch (className) {
+            case "java.util.ArrayList" -> new ArrayList<>();
+            case "java.util.HashSet" -> new HashSet<>();
+            case "java.util.LinkedHashSet" -> new LinkedHashSet<>();
+            case "java.util.LinkedList" -> new LinkedList<>();
+            case "java.util.TreeSet" -> new TreeSet<>();
+            default -> throw new IllegalArgumentException("Cannot merge with class " + className);
+        };
 
         merged.addAll(c1);
         merged.addAll(c2);
--- src/test/java/io/huangsam/trial/universal/TestMerger.java
+++ src/test/java/io/huangsam/trial/universal/TestMerger.java
@@ -2,10 +2,11 @@ package io.huangsam.trial.universal;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -22,17 +23,17 @@ public class TestMerger {
     private static final Collection<Integer> C2 = Arrays.asList(1, 10, 4);
 
     @Test
-    void testMergeAsList() {
-        Collection<Integer> mergedSame = MERGER.mergeAs(C1, C1, List.class);
-        Collection<Integer> mergedDifferent = MERGER.mergeAs(C1, C2, List.class);
+    void testMergeAsList() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Collection<Integer> mergedSame = MERGER.mergeAs(C1, C1, ArrayList.class);
+        Collection<Integer> mergedDifferent = MERGER.mergeAs(C1, C2, ArrayList.class);
         assertEquals(C1.size() + C1.size(), mergedSame.size());
         assertEquals(C1.size() + C2.size(), mergedDifferent.size());
     }
 
     @Test
-    void testMergeAsSet() {
-        Collection<Integer> mergedSame = MERGER.mergeAs(C1, C1, Set.class);
-        Collection<Integer> mergedDifferent = MERGER.mergeAs(C1, C2, Set.class);
+    void testMergeAsSet() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Collection<Integer> mergedSame = MERGER.mergeAs(C1, C1, HashSet.class);
+        Collection<Integer> mergedDifferent = MERGER.mergeAs(C1, C2, HashSet.class);
         assertEquals(C1.size(), mergedSame.size());
         assertEquals(C1.size() + C2.size(), mergedDifferent.size());
     }
