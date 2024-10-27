640b709020a944999a43c1899a42bf02fca6dc49
Samuel Huang <samhuang91@gmail.com>
2024-10-26 17:20:38 -0700

    Add test for Set interface

--- src/test/java/io/huangsam/trial/collection/TestSet.java
+++ src/test/java/io/huangsam/trial/collection/TestSet.java
@@ -1,27 +1,16 @@
 package io.huangsam.trial.collection;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestSet {
-    private final Set<Integer> evenSet = new HashSet<>();
-    private final Set<Integer> oddSet = new HashSet<>();
-
-    @BeforeEach
-    void resetSet() {
-        evenSet.clear();
-        evenSet.addAll(Arrays.asList(2, 4, 6, 8, 10));
-
-        oddSet.clear();
-        oddSet.addAll(Arrays.asList(1, 3, 5, 7, 9));
-    }
+    private final Set<Integer> evenSet = Set.of(2, 4, 6, 8);
+    private final Set<Integer> oddSet = Set.of(1, 3, 5, 7);
 
     @Test
     void testContainsForExistence() {
