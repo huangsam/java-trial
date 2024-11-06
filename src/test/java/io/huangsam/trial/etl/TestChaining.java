536251bc9f814f14a50161c9e4c80981948394db
Samuel Huang <samhuang91@gmail.com>
2024-11-05 20:51:46 -0800

    Add new test for stream chaining

--- src/test/java/io/huangsam/trial/etl/TestChaining.java
+++ src/test/java/io/huangsam/trial/etl/TestChaining.java
@@ -1,16 +1,20 @@
 package io.huangsam.trial.etl;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TestFlakyMath {
+public class TestChaining {
     @ParameterizedTest
     @ValueSource(longs = {1L, 2L, 3L, 5L})
     void testOptionalChainingOnLittleNumber(long smallish) {
@@ -25,6 +29,15 @@ public class TestFlakyMath {
         Optional<Long> optional = getOptionalChain(biggish);
         assertTrue(optional.isPresent());
         assertEquals(biggish * 6L, optional.get());
+    }
+
+    @Test
+    void testStreamChainingOnNestedIntegers() {
+        int sum = Stream.of(List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9))
+                .flatMap(Collection::stream)
+                .mapToInt(Integer::intValue).sum();
+
+        assertEquals(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9, sum);
     }
 
     private Optional<Long> getOptionalChain(Long value) {
