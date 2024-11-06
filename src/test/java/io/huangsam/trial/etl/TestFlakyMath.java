f7b2a12fe99a3e18767551d0a4e5a1379d173c04
Samuel Huang <samhuang91@gmail.com>
2024-11-05 20:03:45 -0800

    Add qualifications to tests

--- src/test/java/io/huangsam/trial/etl/TestFlakyMath.java
+++ src/test/java/io/huangsam/trial/etl/TestFlakyMath.java
@@ -3,21 +3,28 @@ package io.huangsam.trial.etl;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestFlakyMath {
     @ParameterizedTest
     @ValueSource(longs = {1L, 2L, 3L, 5L})
     void testOptionalChainingOnLittleNumber(long smallish) {
-        assertTrue(getOptionalChain(smallish).isEmpty());
+        Optional<Long> optional = getOptionalChain(smallish);
+        assertTrue(optional.isEmpty());
+        assertThrows(NoSuchElementException.class, optional::get);
     }
 
     @ParameterizedTest
     @ValueSource(longs = {8L, 13L, 21L, 34L})
     void testOptionalChainingOnBigNumber(long biggish) {
-        assertTrue(getOptionalChain(biggish).isPresent());
+        Optional<Long> optional = getOptionalChain(biggish);
+        assertTrue(optional.isPresent());
+        assertEquals(biggish * 6L, optional.get());
     }
 
     private Optional<Long> getOptionalChain(Long value) {
