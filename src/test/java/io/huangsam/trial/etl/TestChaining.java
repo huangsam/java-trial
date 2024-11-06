42329b396c29c6c0689c5bebfe88d0dc3105496b
Samuel Huang <samhuang91@gmail.com>
2024-11-06 00:03:57 -0800

    Create a test for arrays

--- src/test/java/io/huangsam/trial/etl/TestChaining.java
+++ src/test/java/io/huangsam/trial/etl/TestChaining.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,6 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestChaining {
+    private static final int SUM_TO_NINE = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9).sum();
+
     @ParameterizedTest
     @ValueSource(longs = {1L, 2L, 3L, 5L})
     void testOptionalChainingOnLittleNumber(long smallish) {
@@ -32,13 +36,22 @@ public class TestChaining {
     }
 
     @Test
-    void testStreamChainingOnNestedIntegers() {
-        int total = Stream.of(List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9))
+    void testStreamChainingOnNestedIntegerList() {
+        int listTotal = Stream.of(List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9))
                 .flatMap(Collection::stream)
                 .mapToInt(Integer::intValue)
                 .sum();
 
-        assertEquals(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9, total);
+        assertEquals(SUM_TO_NINE, listTotal);
+    }
+
+    @Test
+    void testStreamChainingOnNestedIntegerArray() {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        int matrixTotal = Arrays.stream(matrix).flatMapToInt(Arrays::stream).sum();
+
+        assertEquals(SUM_TO_NINE, matrixTotal);
     }
 
     private Optional<Long> getOptionalChain(Long value) {
